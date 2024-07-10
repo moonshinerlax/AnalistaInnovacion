1fe73fefb7c5fe1d0c7c1a07ab89c3a4414d5133
Marco Ivan Nava <53541443+moonshinerlax@users.noreply.github.com>
2024-07-10 09:32:16 -0600

    Delete innova-tube/src/environments directory

--- innova-tube/src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiKey: 'prodKey',
-  RECAPTCHA_SECRET_KEY: '6LferQsqAAAAAIfhOUMy4RWaVx92yu2HRuRum8zp',
-  YOUTUBE_API_KEY: 'AIzaSyC5QUCSRwtBMUdSvWlgTjye5XHgIFV-q4c'
-};
