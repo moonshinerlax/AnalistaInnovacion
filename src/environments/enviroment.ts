14add0df35970bbb384acc9ebd4b54a26ed58f1a
Ivan Nava <aivenpromedia@gmail.com>
2024-07-11 00:38:24 -0600

    1

--- src/environments/enviroment.ts
+++ src/environments/enviroment.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://innova-tube-livid.vercel.app/'  // Local development URL
+  apiUrl: 'https://innova-tube-livid.vercel.app/',
+  YOUTUBE_API_KEY: 'AIzaSyC5QUCSRwtBMUdSvWlgTjye5XHgIFV-q4c',
+  RECAPTCHA_SECRET_KEY: '6LferQsqAAAAAIfhOUMy4RWaVx92yu2HRuRum8zp',
+  RECAPTCHA_KEY: '6LcTvgsqAAAAAIrebSy2Y7Q0jtF3b_Twlb_ZiqX9'
 };
