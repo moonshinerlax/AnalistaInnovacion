3aa58335b92caf4435037f70afa3179a0e555245
Ivan Nava <aivenpromedia@gmail.com>
2024-07-10 23:30:37 -0600

    5

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,25 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 const authroutes = require('./routes/authroutes');
 const videoroutes = require('./routes/videoroutes');
 const { connectDB } = require('./db');
 
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'dist/innova-tube')));
+
+prerenderedRoutes.forEach(route => {
+  app.get(route, (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/innova-tube', 'index.html'));
+  });
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/innova-tube/index.html'));
+});
 const port = process.env.PORT || 3000;
 connectDB();
 
